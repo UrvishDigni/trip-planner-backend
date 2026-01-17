6bdcd950b892b5a5607f300b7443f181d165fb77
Urvish <urvish.shiyani@weenggs.in>
2026-01-17 19:38:50 +0530

    chore: Update CORS middleware to explicitly define origin, methods, and allowed headers.

--- server.ts
+++ server.ts
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.BACKEND_PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 // Routes
